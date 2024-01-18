c03ef4dc88a51b0d4599d9151747cfeb47d232b9
miksor2023 <miksor@yandex.ru>
2024-01-18 14:11:14 +0300

    Initial commit

---
+++ src/main/java/com/pro/sky/cours2/homework/hw150124/Hw150124Application.java
@@ -0,0 +1,13 @@
+package com.pro.sky.cours2.homework.hw150124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw150124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw150124Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/sky/cours2/homework/hw150124/Hw150124ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.sky.cours2.homework.hw150124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw150124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
